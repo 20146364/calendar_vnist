7f0ae9a675796c873ce06d6622c4c75c9bbdf6b8
Hung Phan <hungpt.hust@gmail.com>
2018-12-28 16:41:49 +0700

    update dialog ver 2

--- src/app/newtodo/newtodo.component.ts
+++ src/app/newtodo/newtodo.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Time {
+  name: string;
+}
+
 @Component({
   selector: 'app-newtodo',
   templateUrl: './newtodo.component.html',
   styleUrls: ['./newtodo.component.scss']
 })
+
+
+
 export class NewtodoComponent implements OnInit {
 
   title = 'New todo';
+  val = 1;
+  time: Time[];
+  selectedTime: Time;
 
-  constructor() { }
+  constructor() {
+    this.time = [
+      {name: 'week'},
+      {name: 'month'},
+      {name: 'year'}
+    ];
+  }
 
   ngOnInit() {
   }
