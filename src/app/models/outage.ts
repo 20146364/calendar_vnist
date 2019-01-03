99b317354159b1faaa03b5fce8007854c2672916
Thuy Dang Nhu <thuydn.vnist@gmail.com>
2019-01-03 16:10:53 +0700

    add outage dialog and fix filter participating people

--- src/app/models/outage.ts
+++ src/app/models/outage.ts
@@ -9,13 +9,14 @@ export class Outage implements IEvent {
     color: string;
     className: string;
     plantID: number;
-    _plantName: string;
+    _plantName: any;
     typeOfEvent = "Outage";
 
     expectedBegin: string;
     expectedEnd: string;
     comment: any;
-    tsticketID: string;
+    tsticketID: any;
+    _ticketName: any;
     outageCategory: string;
 
     eventOutage: any;
@@ -26,6 +27,7 @@ export class Outage implements IEvent {
     }
 
     initInfo(ot: any) {
+        console.log('ot1:', ot);
         this.color = "#dc5e14";
         this.eventOutage = ot;
         this.id = ot.id;
@@ -33,10 +35,12 @@ export class Outage implements IEvent {
         this.start = new Date(ot.begin ? ot.begin : ot.expectedbegin);
         this.end = new Date(ot.end ? ot.end : ot.expectedend);
         this.plantID = ot.plant_id;
-        this.expectedBegin = (new Date(ot.expectedbegin)).toLocaleString();
-        this.expectedEnd = (new Date(ot.expectedend)).toLocaleString();
+        this.plantName = ot.plant_id;
+        this.expectedBegin = (new Date(ot.expectedbegin ? ot.expectedbegin : ot.begin)).toLocaleString();
+        this.expectedEnd = (new Date(ot.expectedend ? ot.expectedend : ot.end)).toLocaleString();
         this.comment = ot.comment;
         this.tsticketID = ot.tsticket_id;
+        this.ticketName = ot.tsticket_id;
         this.outageCategory = ot.tsoutagecategory.name;
         if (this.end.getTime() - this.start.getTime() >= 86400000) {
             this.allDay = true;
@@ -44,7 +48,61 @@ export class Outage implements IEvent {
     }
 
     getInfo(ot: any){
-        
+        console.log('ot2:', ot);
+        this.id = ot.id;
+        this.title = ot.title;
+        this.start = ot.start;
+        this.end = ot.end;
+        this.plantID = ot.extendedProps.plantID;
+        this._plantName = ot.extendedProps._plantName;
+        this.expectedBegin = ot.extendedProps.expectedBegin;
+        this.expectedEnd = ot.extendedProps.expectedEnd;
+        this.comment = ot.extendedProps.comment;
+        this.tsticketID = ot.extendedProps.tsticketID;
+        this._ticketName = ot.extendedProps._ticketName;
+        this.outageCategory = ot.extendedProps.outageCategory;
+    }
+
+    get plantName() {
+        return this._plantName;
+    }
+    set plantName(plantID: any) {
+        let myItem: any;
+        let key;
+        let listPlant: any[];
+        key = 'localPlant';
+        myItem = sessionStorage.getItem(key);
+        if (myItem !== 'undefined') {
+            myItem = JSON.parse(myItem);
+            listPlant = myItem;
+        }
+        if (listPlant) {
+            this._plantName = listPlant.find( e => e.id == plantID);
+            if (this._plantName !== undefined) {
+                this._plantName = this._plantName.name;
+            }
+        }
+    }
+    
+    get ticketName() {
+        return this._ticketName;
+    }
+    set ticketName(ticketID: any) {
+        let myItem: any;
+        let key;
+        let listTicket: any[];
+        key = 'localTicket';
+        myItem = sessionStorage.getItem(key);
+        if (myItem !== 'undefined') {
+            myItem = JSON.parse(myItem);
+            listTicket = myItem;
+        }
+        if (listTicket) {
+            this._ticketName = listTicket.find( e => e.id == ticketID);
+            if (this._ticketName !== undefined) {
+                this._ticketName = this._ticketName.name;
+            }
+        }
     }
 
   }
